33ab2198c3de83123001d59ad63f730ede8a9835
Pablo Zarate <pablozarate_lol@hotmail.com>
2020-05-04 15:39:13 -0500

    Auth index

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const UserToken = require('../models/userToken.model')
+var colors = require('colors');
 //GET My USER
 exports.findMe = async (req, res, next) => {
     const userToken = req.headers.authorization.split(" ")[1];
@@ -7,6 +8,7 @@ exports.findMe = async (req, res, next) => {
     .then(user => {
         res.status(200).json(user._id)
     }).catch(err => {
+        console.log("unauthorized".black.bgRed).
         res.status(401).json({err})
     });
 }
