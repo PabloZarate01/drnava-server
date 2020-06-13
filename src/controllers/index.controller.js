4d0f95d69ce371aa51b902a7e0c2281f8e3e09be
Pablo Zarate <pablozarate_lol@hotmail.com>
2020-06-13 11:27:00 -0500

    Auth changes

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,13 +2,13 @@ const bcrypt = require('bcryptjs')
 const UserToken = require('../models/userToken.model')
 var colors = require('colors');
 //GET My USER
-exports.findMe = async (req, res, next) => {
+exports.findMe = (req, res, next) => {
     const userToken = req.headers.authorization.split(" ")[1];
-    await UserToken.findOne({userToken})
+    UserToken.findOne({userToken})
     .then(user => {
         res.status(200).json(user._id)
     }).catch(err => {
         console.log("unauthorized".black.bgRed).
-        res.status(401).json({err})
+        res.status(402).json({err})
     });
 }
